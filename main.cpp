a887c1c1e63836a961497e03884024b2f88e4953
IamDoni <130747242+IamDoni@users.noreply.github.com>
2024-03-24 16:34:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,31 @@ using namespace std;
 
 string input_filename="demo.txt";
 
+void show_argument() {
+	ofstream outFile("show_argument.txt");
 
+	if (!outFile.is_open()) {
+		std::cerr << "µLªk¥´¶}ÀÉ®×" << std::endl;
+		return;
+	}
+	
+	outFile << "Alpha  " << alpha << endl;
+	outFile << "Beta  " << beta << endl;
+	outFile << "Gamma  " << gamma << endl;
+	outFile << "Delta  " << delta << endl;
+	outFile << "Bin_width  " << bin_width << endl;
+	outFile << "Bin_height  " << bin_height << endl;
+	outFile << "Bin_max_util  " << bin_max_util << endl;
+	outFile << "Row_start_x  " << row_start_x << endl;
+	outFile << "Row_start_y  " << row_start_y << endl;
+	outFile << "Row_width  " << row_width << endl;
+	outFile << "Row_height  " << row_height << endl;
+	outFile << "Displacement_delay  " << displacement_delay << endl;
+
+	
+	outFile.close();
+	return;
+}
 void input_file();
 void initialize();
 void show();
