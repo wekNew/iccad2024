60d61c5027517f59907c903132b4e48eadca0806
Junyan920710 <161047054+Junyan920710@users.noreply.github.com>
2024-07-13 19:05:35 +0800

    connect other legalize.h

--- legalize.h
+++ legalize.h
@@ -10,48 +10,29 @@
 #include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
+#include"Cell.h"
+#include"Point.h"
+#include"Partition.h"
 
 using namespace std;
 
 // Define structures used in legalize.cpp
-
-struct cell {
-    int bit;
-    double slack;
-    int width;
-    int height;
-};
-
-struct cluster {
-    int bit;
-    double slack;
-    double x_ordinate, y_ordinate;
-    std::vector<cell> C;
-    int cluster_name;
-    int p_right;
-    int p_left;
-    int p_up;
-    int p_down;
-};
-
-
-
 // Function prototypes
 
-void to_the_site(double& x, double& y);
+void to_the_site(int& x, int& y);
 void print_overlap_graph(const unordered_map<int,unordered_set<int>>& overlap_graph);
 void print_edges(const set<pair<int, int>>& edges);
-void find_vertical_overlaps(vector<cluster>& clusters,unordered_map<int, unordered_set<int>>& overlap_graph);
-void find_horizontal_overlaps(vector<cluster>& clusters, unordered_map<int, unordered_set<int>>& overlap_graph);
+void find_vertical_overlaps(vector<Cell>& clusters,unordered_map<int, unordered_set<int>>& overlap_graph);
+void find_horizontal_overlaps(vector<Cell>& clusters, unordered_map<int, unordered_set<int>>& overlap_graph);
 void creat_overlap_edge(unordered_map<int,unordered_set<int>>& graph, int direction);
-void merge_horizontal_vertical_overlap(const set<pair<int, int>>& edges1, const set<pair<int, int>>& edges2, set<pair<int, int>>& overlap_edges, vector<pair<cluster, cluster>>& need_move_overlap_edges,set<int>& overlap_cluster,vector<cluster> clusters);
-bool poriority_bigger(const cluster& cluster1, const cluster& cluster2);
-bool check_overlap(const cluster& cluster1,const  cluster& cluster2);
-vector<cluster> find_nearest_overlap_cluster(cluster& current_cluster,vector<cluster> overlap_clusters);
-bool samewidth( cluster& cluster1, cluster& cluster2);
-void move_cluster(cluster& current_cluster, const vector<cluster>& overlap_clusters, vector<cluster>& clusters);
-queue<cluster> get_overlap_clusters(vector<cluster>& clusters,vector<pair<cluster, cluster>>& need_move_overlap_edges,vector<cluster>& overlap_clusters);
-void shift_until_legal(vector<cluster> overlap_clusters, queue<cluster>& overlap_queue, vector<cluster>& clusters, vector<pair<cluster, cluster>>& need_move_overlap_edges);
-void legalize(vector<cluster>& clusters);
+//void merge_horizontal_vertical_overlap( set<pair<int, int>>& edges1,  set<pair<int, int>>& edges2, set<pair<int, int>>& overlap_edges, vector<pair<Cell, Cell>>& need_move_overlap_edges,set<int>& overlap_cluster,vector<Cell> clusters);
+bool poriority_bigger( Cell& cluster1, Cell& cluster2);
+bool check_overlap(const Cell& cluster1,const  Cell& cluster2);
+vector<Cell> find_nearest_overlap_cluster(Cell& current_cluster,vector<Cell> overlap_clusters);
+bool samewidth(Cell& cluster1, Cell& cluster2);
+void move_cluster(Cell& current_cluster, const vector<Cell>& overlap_clusters, vector<Cell>& clusters);
+queue<Cell> get_overlap_clusters(vector<Cell>& clusters,vector<pair<Cell, Cell>>& need_move_overlap_edges,vector<Cell>& overlap_clusters);
+void shift_until_legal(vector<Cell> overlap_clusters, queue<Cell>& overlap_queue, vector<Cell>& clusters, vector<pair<Cell, Cell>>& need_move_overlap_edges);
+void legalize(vector<Cell>& clusters);
 
 #endif // LEGALIZE_H
