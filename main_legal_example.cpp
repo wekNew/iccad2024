91cf536ff97dca8daf984fba32683774f00f797d
Junyan920710 <161047054+Junyan920710@users.noreply.github.com>
2024-07-13 19:12:37 +0800

    Update main_legal_example.cpp

--- main_legal_example.cpp
+++ main_legal_example.cpp
@@ -1,43 +1,22 @@
 #include "legalize.h"
+#include"Cell.h"
 
 using namespace std;
 
 
 int main() {
-	//Åª¨úpartition//
 
-	/////////////////
-
-	//MBFF¡@cell³]©w//
-	//1bit slack:1.0 height:10.0 width:5.0
-	//2bit slack:2.0 height:10.0 width:8.0
-	//4bit slack:3.0 height:20.0 width:12.0	
-	vector<cell>cells;
-	cell bit_MBFF_1 = { 1, 1.0, 5, 10 };
-	cell bit_MBFF_2 = { 2, 2.0, 8, 10 };
-	cell bit_MBFF_4 = { 4, 3.0, 12, 20 };
-
-	cells.push_back(bit_MBFF_1);
-	cells.push_back(bit_MBFF_2);
-	cells.push_back(bit_MBFF_4);
-	//////////////////
-
-	//cluster/////////
-	//C0:c11, C1:c4,c20,c24,c26, C2:c2,c13,c17,c22, C3:c5 C4:c16,c21,
-	//C5:c7,c14,c18,c28, C6:c1,c10, C7:c0,c3,c23,c27, C8:c6,c9,c15,c29, C9:c8,c12,c19,c25
-	vector<cluster> clusters = { //bit,slack,x_ordinate,y_ordinate,cell_kind,cluster_name
-		{1, 1.0, 0.0, 10.0, {bit_MBFF_1}, 0},
-		{4, 3.0, 8.0, 20.0, {bit_MBFF_4}, 1},
-		{4, 3.0, 0.0, 0.0, {bit_MBFF_4}, 2},
-		{1, 1.0, 10.0, 90.0, {bit_MBFF_1}, 3},
-		{2, 2.0, 6.0, 20.0, {bit_MBFF_2}, 4},
-		{4, 3.0, 0.0, 0.0, {bit_MBFF_4}, 5},
-		{2, 2.0, 14.0, 20.0, {bit_MBFF_2}, 6},
-		{4, 3.0, 0.0, 50.0, {bit_MBFF_4}, 7},
-		{4, 3.0, 35.0, 40.0, {bit_MBFF_4}, 8},
-		{4, 3.0, 30.0, 80.0, {bit_MBFF_4}, 9}
-	};
-	//////////////////
-	legalize(clusters);//¥u»İ­n§âpartionÂà´«¦¨clusterªº§Î¦¡§Y¥ilegalize~~
+	vector<Cell> MBFF;
+	MBFF.push_back({ 1, 5, 10, 0, 0, 0, 0, 0, 0, 0 });
+	MBFF.push_back({4, 12, 20, 5, 8, 0, 0, 0, 0, 1});
+	MBFF.push_back({ 4, 12, 20, 6, 10, 0, 0, 0, 0, 2 });
+	MBFF.push_back({1, 5, 10, 7, 20, 0, 0, 0, 0, 3});
+	MBFF.push_back({ 2, 8, 10, 9, 0, 0, 0, 0, 0, 4 });
+	MBFF.push_back({ 4, 12, 20, 10, 10, 0, 0, 0, 0, 5 });
+	MBFF.push_back({ 2, 8, 10, 50, 50, 0, 0, 0, 0, 6 });
+	MBFF.push_back({ 4, 12, 20, 0, 10, 0, 0, 0, 0, 7 });
+	MBFF.push_back({ 4, 12, 20, 0, 20, 0, 0, 0, 0, 8 });
+	MBFF.push_back({ 4, 12, 20, 10, 0, 0, 0, 0, 0, 9 });
+	legalize(MBFF);//åªéœ€è¦æŠŠpartionè½‰æ›æˆclusterçš„å½¢å¼å³å¯legalize~~
 	return 0;
  }
