83aabf49fabfa0b273d1a3db9196d9261bfdad37
Junyan920710 <161047054+Junyan920710@users.noreply.github.com>
2024-08-05 18:03:22 +0800

    Update tetris.h

--- tetris.h
+++ tetris.h
@@ -2,6 +2,7 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 #include <vector>
+#include<queue>
 #include <algorithm>
 #include "cell.h"
 
@@ -11,7 +12,7 @@ struct Interval {
 };
 struct IntervalPoint {
 	float x_pos;
-	int identity;//		1�N��start	-1�N��end;
+	int identity;//		1代表start	-1代表end;
 };
 struct OnsiteLocation {
 	int startX;
@@ -25,6 +26,7 @@ class Window {
 private:
 	int x_pos, y_pos, height, width;
 	vector<shared_ptr<Cell>> FF;
+	vector<shared_ptr<Cell>> illegal_FF;
 	vector<shared_ptr<Cell>> Gate;
 	vector<shared_ptr<Cell>> EdgeCell;
 	vector<vector<Interval>> row_interval;
@@ -51,21 +53,31 @@ private:
 			}
 		}
 		std::sort(temp.begin(), temp.end(), [](const IntervalPoint& a, const IntervalPoint& b) {
-			return a.x_pos < b.x_pos;
+				return a.x_pos < b.x_pos;
 			});
 		int count = 0;
 		bool record = false;
 		Interval current_interval;
 
 
-		for (auto v : temp) {
-			count += v.identity;
-			if (count == row_height) {
-				current_interval.start = v.x_pos;
+		for (int i = 0;i<temp.size();i++) {
+			count += temp[i].identity;
+			if (temp[0].x_pos != x_pos) {//存最前面的區間
+				current_interval.start = x_pos;
+				current_interval.end = temp[0].x_pos;
+				final_interval.emplace_back(current_interval);
+			}
+			if (temp[temp.size()-1].x_pos != (x_pos+width)) {//存最後面的區間
+				current_interval.start = temp[temp.size() - 1].x_pos;
+				current_interval.end =( x_pos + width );
+				final_interval.emplace_back(current_interval);
+			}
+			if (count == 0) {
+				current_interval.start = temp[i].x_pos;//存的是V.end
 				record = true;
 			}
-			if (count == row_height - 1 && record) {
-				current_interval.end = v.x_pos;
+			if (count ==  1 && record) {
+				current_interval.end = temp[i].x_pos;//存的是V.start
 				final_interval.emplace_back(current_interval);
 				record = false;
 			}
@@ -95,9 +107,9 @@ public:
 		return EdgeCell;
 	}
 	vector<Interval> get_valid_interval(int current_row, int row_height) {
-		if (row_height == 1) {
+		/*if (row_height == 1) {
 			return row_interval.at(current_row);
-		}
+		}*/
 		return intersection(current_row, row_height);
 	}
 
@@ -108,6 +120,13 @@ public:
 		this->width = width;
 		this->height = height;
 	}
+	void set_row_interval(int row, float x_start, float x_end) {
+		Interval new_interval = { x_start, x_end };
+		row_interval[row].emplace_back(new_interval);
+	}
+	void set_illegal_FF(shared_ptr<Cell>& FF) {
+		illegal_FF.emplace_back(FF);
+	}
 
 };
 inline int IndexConvert(float pos, int window_start_pos, int unit) {
@@ -116,6 +135,9 @@ inline int IndexConvert(float pos, int window_start_pos, int unit) {
 shared_ptr<Window> FindWindowForMBFF(shared_ptr<Cell> MBFF, vector<vector<shared_ptr<Window>>> windows, int window_start_x, int window_start_y, int unit_x, int unit_y);
 shared_ptr<Window> CombiWindow(vector<vector<shared_ptr<Window>>> windows, int x_start_index, int y_start_index, int x_end_index, int y_end_index);
 
-
-
-#endif
+//vector<Cell>already_place_MBFF;//放已經放好的MBFF
+void to_the_site(shared_ptr<Cell>& cell);
+//float poriority_cost_function(Cell& cluster1, Cell& cluster2);
+//bool poriority_bigger(Cell& cluster1, Cell& cluster2);
+void tetris(vector<Window>& WD, vector<Cell>& clusters, int bin_width, int bin_height, int die_x_min, int die_y_min, int die_x_max, int die_y_max);
+#endif // TERTIS_H;
